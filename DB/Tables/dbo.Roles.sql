7be06e7b618addc97a62157bea2f586da9e94bfd
unbelivable11 <rpgeha@gmail.com>
2025-02-24 13:31:27 +0200

    DB

---
+++ DB/Tables/dbo.Roles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Roles] (
+  [Id] [int] IDENTITY,
+  [Name] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+GO
---
+++ DB/Tables/dbo.Users.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Users] (
+  [Id] [int] IDENTITY,
+  [Login] [nvarchar](50) NOT NULL,
+  [PasswordHash] [nvarchar](100) NOT NULL,
+  [Email] [nvarchar](max) NOT NULL,
+  [RoleId] [int] NOT NULL,
+  CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE INDEX [IX_Users_RoleId]
+  ON [dbo].[Users] ([RoleId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Users]
+  ADD CONSTRAINT [FK_Users_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Roles] ([Id]) ON DELETE CASCADE
+GO
